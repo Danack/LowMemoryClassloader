c8c5a7881919cdf708044dc39b25f2fdd58e775b
Danack <Danack@basereality.com>
2013-09-10 16:00:19 +0100

    Initial commit. Moved from a separate project into it's on, for people to use.

---
+++ LowMemoryClassloader.php
@@ -0,0 +1,142 @@
+<?php
+
+/*
+ * This file was part of Composer, and still depends on Composer to function.
+ *
+ * (c) Nils Adermann <naderman@naderman.de>
+ *     Jordi Boggiano <j.boggiano@seld.be>
+ *     Dan Ackroyd <Danack@basereality.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Intahwebz\Autoload;
+
+
+class LowMemoryClassloader
+{
+    private $classMap = null;
+    private $prefixes = array();
+
+    function __construct() {
+        $filepath = dirname(dirname(dirname(__DIR__))).'/composer/autoload_namespaces.php';
+        $map = require $filepath;
+
+        foreach ($map as $namespace => $path) {
+            $this->set($namespace, $path);
+        }
+
+        $this->register(true);
+    }
+    
+    
+    /**
+     * Registers a set of classes, replacing any others previously set.
+     *
+     * @param string       $prefix The classes prefix
+     * @param array|string $paths  The location(s) of the classes
+     */
+    public function set($prefix, $paths) {
+        $this->prefixes[substr($prefix, 0, 1)][$prefix] = (array) $paths;
+    }
+
+    /**
+     * Registers this instance as an autoloader.
+     *
+     * @param bool $prepend Whether to prepend the autoloader or not
+     */
+    public function register($prepend = false)
+    {
+        spl_autoload_register(array($this, 'loadClass'), true, $prepend);
+    }
+
+    /**
+     * Unregisters this instance as an autoloader.
+     */
+    public function unregister()
+    {
+        spl_autoload_unregister(array($this, 'loadClass'));
+    }
+
+    /**
+     * Loads the given class or interface.
+     *
+     * @param  string    $class The name of the class
+     * @return bool|null True if loaded, null otherwise
+     */
+    public function loadClass($class)
+    {
+        if ($file = $this->findFile($class)) {
+            include $file;
+
+            return true;
+        }
+    }
+
+    /**
+     * Finds the path to the file where the class is defined.
+     *
+     * @param string $class The name of the class
+     *
+     * @return string|false The path if found, false otherwise
+     */
+    public function findFile($class)
+    {
+        if (false !== $pos = strrpos($class, '\\')) {
+            // namespaced class name
+            $classPath = strtr(substr($class, 0, $pos), '\\', DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+            $className = substr($class, $pos + 1);
+        } else {
+            // PEAR-like class name
+            $classPath = null;
+            $className = $class;
+        }
+
+        $classPath .= strtr($className, '_', DIRECTORY_SEPARATOR) . '.php';
+
+        $first = $class[0];
+        if (isset($this->prefixes[$first])) {
+            foreach ($this->prefixes[$first] as $prefix => $dirs) {
+
+                //Check all possible paths in OPCache before checking the file system
+                if (0 === strpos($class, $prefix)) {
+                    foreach ($dirs as $dir) {
+                        $filename = $dir.DIRECTORY_SEPARATOR.$classPath;
+                        if (opcache_script_cached($filename) == true) {
+                            return $filename;
+                        }
+                    }
+
+                    foreach ($dirs as $dir) {
+                        $filename = $dir.DIRECTORY_SEPARATOR.$classPath;
+                        if (file_exists($filename)) {
+                            return $filename;
+                        }
+                    }
+                }
+            }
+        }
+        
+        //Didn't find it - might be a classmap
+        if ($this->classMap == null) {
+            $filepath = dirname(dirname(dirname(__DIR__))).'/composer/autoload_classmap.php';
+            $this->classMap = require $filepath;
+        }
+
+        if (isset($this->classMap[$class])) {
+            return $this->classMap[$class];
+        }
+
+        return false;
+    }
+}
+
+
+$loader = new \Intahwebz\Autoload\LowMemoryClassloader();
+
+return $loader;
+
+
+
+ 
--- README.md
+++ README.md
@@ -2,3 +2,48 @@ LowMemoryClassloader
 ====================
 
 A PHP Composer compatible classloader that uses less memory.
+
+
+Why
+===
+
+I'm an ex-video games developer. I can't stand seeing huge chunks of memory being used when it isn't necessary.
+
+After running `composer--optimize-autoloader update` on my current project, the 'optimised' version of the class loader looks like this: http://pastebin.com/Xa1ii7PY which quite frankly appalls. It takes up 645kB of memory, which is about 30% of the memory needed to display a page in the framework I use.
+
+Although sort of optimization for figuring out where the files for classes exist is needed (as file_exists() calls are too darn slow, and must be avoided at all costs) we really ought to do better than using up that much memory.
+
+
+How it works
+============
+
+OPCache stores optimized versions of PHP files. We can use that as a cache of which files have already been loaded in a previous PHP script, which effectively gives us a cache of what files contain which classes from a small list of available possibilities.
+
+So instead of storing a massive list of where all the individual file for classes are, we can just use the much shorter list of top-level namespaces (from the file autoload_namespaces.php) to search for files once, and then, effectively, cache the result through OPCache.
+
+The only time when the full classmap file is loaded is when PHP tries to autoload a class that isn't cached by OPCache, and isn't namespaced.
+
+The LowMemoryClassloader requires you to use my fork of OPCache from https://github.com/Danack/ZendOptimizerPlus as it adds the function `opcache_script_cached($scriptName)` to allow you to check if a script is cached in OPCache.
+
+
+How you use it
+==============
+
+1. Compile in the modified version of OPCache.
+
+2. In your PHP script wherever you include the Composer classloader: 
+
+    require_once('../vendor/autoload.php');
+    
+replace it with the LowMemoryClassLoader:
+
+    require_once('../vendor/intahwebz/lowmemoryclassloader/LowMemoryClassloader.php');
+    
+3. There is no step 3.
+
+
+Behaviour difference to Composer
+================================
+
+The only known difference in behaviour to the Composer autoloader is that when the LowMemoryClassloader fails to load the file for a class, it does not cache that fact, and will try to find the class again, on the next attempt to load it. This is the correct behaviour IMHO, as it makes runtime code generation be easier to handle.
+ 
